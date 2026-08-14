87b9a6303129f4c8a201b974c4e4627e0c756fee
Ali-jafari <mmdddpdd@gmail.com>
2026-08-14 14:06:06 +0330

    feat(group-settings): add BOT_CREATOR_ID and defaults

--- config.py
+++ config.py
@@ -15,6 +15,9 @@ AI_MODEL = os.getenv(
     "llama-3.3-70b-versatile"
 )
 
+# ID of the bot creator who can enable/disable the bot globally per-group
+BOT_CREATOR_ID = int(os.getenv("BOT_CREATOR_ID", "6627527892"))
+
 PERSONAL_MIN_MESSAGES = int(
     os.getenv("PERSONAL_MIN_MESSAGES", "10")
 )
